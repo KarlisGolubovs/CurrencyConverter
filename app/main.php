3048a377e087f35168395e51531dec109c619684
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-04-18 20:42:36 +0300

    pd

---
+++ app/main.php
@@ -0,0 +1,29 @@
+<?php
+
+
+require_once 'vendor/autoload.php';
+
+use App\Modules\CurrencyConverter;
+
+$converter = new CurrencyConverter();
+
+// Read user input
+echo 'Enter the amount to convert (e.g. 10.99): ';
+$amount = (float)readline();
+
+echo 'Enter the base currency (e.g. EUR): ';
+$from = strtoupper(readline());
+
+echo 'Enter the target currency (e.g. USD): ';
+$to = strtoupper(readline());
+
+// Convert the amount
+try {
+    $rate = $converter->getExchangeRate($from, $to);
+    $converted = $amount * $rate;
+    printf("%.2f %s = %.2f %s\n", $amount, $from, $converted, $to);
+} catch (\Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+}
+
+
---
+++ app/modules/Converter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Modules;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use SimpleXMLElement;
+use Exception;
+
+
+class CurrencyConverter
+{
+    private Client $client;
+    private array $rates;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->rates = [];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getExchangeRate($from, $to)
+    {
+        $from = strtoupper($from);
+        $to = strtoupper($to);
+
+        if (empty($this->rates)) {
+            try {
+                $response = $this->client->request('GET', 'https://www.latvijasbanka.lv/vk/ecb.xml');
+            } catch (GuzzleException $e) {
+            }
+            $xml = new SimpleXMLElement((string) $response->getBody());
+
+            foreach ($xml->Cube->Cube->Cube as $cube) {
+                $currency = (string) $cube['currency'];
+                $rate = (float) $cube['rate'];
+                $this->rates[$currency] = $rate;
+            }
+
+            $this->rates['EUR'] = 1.0;
+        }
+
+        if (isset($this->rates[$from]) && isset($this->rates[$to])) {
+            return $this->rates[$to] / $this->rates[$from];
+        } else {
+            throw new Exception("Unknown currency: $from or $to");
+        }
+    }
+}
